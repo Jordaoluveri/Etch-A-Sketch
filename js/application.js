68f317d74bec6ea1e731c5997b9c54d34b8fada4
Jordaoluveri <jordaoluveri@gmail.com>
2016-03-29 22:07:27 -0300

    jquery grid done

--- js/application.js
+++ js/application.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
-	
+	var rows = 16;
+	var columns = 16
 
+	for (var r=0; r < columns; r++){
+
+		var $column = $("<div class='column " + r + "'></div>")
+		$("#container").append($column);
+
+		for (var i=0; i < rows; i++){
+
+			var $box = $("<div class='box'></div>")
+			$(".column." + r).append($box);
+			console.log($column);
+		}
+	}
+	
 });
